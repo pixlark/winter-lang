a888394f50414bd8e6a4c6d9ad85b99421d215d2
Paul Tilley <t1000.pixlark@gmail.com>
2018-12-15 18:09:44 -0500

    Fancier error messages: bold + color

--- src/error.c
+++ src/error.c
@@ -2,12 +2,25 @@
 
 #include "common.h"
 
+#define RESET         "\e[0m"
+#define SET_BOLD      "\e[1m"
+#define SET_DIM       "\e[2m"
+#define SET_UNDERLINE "\e[4m"
+#define SET_BLINK     "\e[5m"
+#define SET_INVERTED  "\e[7m"
+#define SET_HIDDEN    "\e[8m"
+
+#define SET_RED "\e[31m"
+
+#define BOLD(x) SET_BOLD x RESET
+#define RED(x)  SET_RED x RESET
+
 void fatal(const char * fmt, ...)
 {
 	va_list args;
 	va_start(args, fmt);
 
-	fprintf(stderr, "encountered error:\n");
+	fprintf(stderr, RED(BOLD("encountered error")) ":\n");
 	vfprintf(stderr, fmt, args);
 	fprintf(stderr, "\n");
 
